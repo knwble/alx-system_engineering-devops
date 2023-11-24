1388b6150f84d9601b47e484fe3abf549372e1dd
knwble <noble4peter1@gmail.com>
2023-11-24 23:32:06 +0100

    install flask

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,11 @@
+# Installs flask 2.1.0
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+# Installs Werkzeug 2.1.1
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
