40de13261b66ddfe5619b09ab71f4da7288c6e37
knwble <noble4peter1@gmail.com>
2023-11-24 23:21:38 +0100

    Install Flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,12 +4,3 @@ package { 'Flask':
   provider => 'pip3',
   require  => Package['python3-pip'], 
 }
-
-# Ensure Python 3 and pip3 are installed
-package { 'python3':
-  ensure => 'installed',
-}
-
-package { 'python3-pip':
-  ensure => 'installed',
-}
